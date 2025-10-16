14b59c53d427a60317531bd4a3619624b1c092ab
NomiH <138275265+NomiH04@users.noreply.github.com>
2025-10-15 22:32:51 -0600

    imagen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 export default function Home() {
   return (
-    <main className="p-8 text-white">
-      <h1 className="text-2xl font-bold">Home</h1>
-      <p className="mt-2">Ir al login:</p>
+    <main className="p-8 text-white flex flex-col items-center">
+      {/* GIF en lugar del título */}
+      <img src="/img/emoji-1-1.gif" alt="Animación de bienvenida"className="w-64 h-64 object-contain" />
+
+      <p className="mt-4">Ir al login:</p>
       <a
         href="/auth/login"
         className="mt-3 inline-block rounded-lg bg-white px-4 py-2 font-semibold text-black"
       >
-        /auth/login
+        Iniciar Sex sion
       </a>
     </main>
   );
